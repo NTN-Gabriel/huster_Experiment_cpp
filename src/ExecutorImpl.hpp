0e48a0af86a0c4f5401ac510832e2653db8e4328
Gabe <3481963967@qq.com>
2024-11-08 18:53:38 +0800

    command to extract independent file

--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -40,57 +40,6 @@ namespace adas
         void TurnRight(void) noexcept;
         void Fast(void) noexcept;
         bool IsFast(void) const noexcept;
-
-    private:
-        class ICommand
-        {
-        public:
-            virtual ~ICommand() = default;
-            virtual void DoOperate(ExecutorImpl &ExecutorImpl) const noexcept = 0;
-        };
-
-        class MoveCommand final : public ICommand
-        {
-        public:
-            void DoOperate(ExecutorImpl &executor) const noexcept override
-            {
-                if (executor.IsFast())
-                    executor.Move();
-                executor.Move();
-            }
-        };
-
-        class TurnLeftCommand final : public ICommand
-        {
-        public:
-            void DoOperate(ExecutorImpl &executor) const noexcept override
-            {
-                if (executor.IsFast())
-                    executor.Move();
-                executor.TurnLeft();
-            }
-        };
-
-        class TurnRightCommand final : public ICommand
-        {
-        public:
-            void DoOperate(ExecutorImpl &executor) const noexcept override
-            {
-                if (executor.IsFast())
-                    executor.Move();
-                executor.TurnRight();
-            }
-        };
-
-        class FastCommand final : public ICommand
-        {
-        public:
-            void DoOperate(ExecutorImpl &executor) const noexcept override
-            {
-                executor.Fast();
-                executor.IsFast();
-            }
-        };
     };
 
 }
